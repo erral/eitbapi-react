ff6d49dcce7f104e7136d9d8627464bc654a31b2
Xabi Ezpeleta <xezpeleta@gmail.com>
2022-12-09 09:37:57 +0100

    Enable HashRouter import library

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import AppRoutes from './Routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
