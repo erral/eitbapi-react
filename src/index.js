53a2781baedffdbac05b7f31a236ce1e64a0427c
Xabi Ezpeleta <xezpeleta@gmail.com>
2022-12-09 09:35:47 +0100

    Enable HashRouter

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <AppRoutes />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 );
